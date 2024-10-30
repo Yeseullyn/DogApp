b051c4ed2f8a5c9eaa1341dc70faf3dc7a027df7
Yeseullyn Lee <LYSL1025@gmail.com>
2024-10-29 22:22:20 -0500

    Added kaminari gem to Gemfile for pagination and increased the number of Breeds and Dogs created in the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ require 'net/http'
 Dog.delete_all
 Breed.delete_all
 
-50.times do
+200.times do
   Breed.create(name: Faker::Creature::Dog.breed)
 end
 
@@ -14,11 +14,11 @@ def fetch_image_url_for_dog
   JSON.parse(response.body)["message"]
 end
 
-200.times do
+300.times do
   Dog.create(
     image_url: fetch_image_url_for_dog,
     breed: Breed.all.sample)
 end
 
-puts "Created #{Breed.count} Breeds"
+puts "Created #{Breed.count} breeds"
 puts "Created #{Dog.count} image of dogs"
