69fbc95b6d430a4a5d805d8628d9f60d99b6dd9e
Yeseullyn Lee <LYSL1025@gmail.com>
2024-10-30 21:34:10 -0500

    Added validates for the DogOwner(join) table.

--- app/models/dog_owner.rb
+++ app/models/dog_owner.rb
@@ -1,4 +1,8 @@
 class DogOwner < ApplicationRecord
   belongs_to :dog
   belongs_to :owner
+  
+  validates :dog_id, presence: true
+  validates :owner_id, presence: true
+  validates :dog_id, uniqueness: { scope: :owner_id }
 end
