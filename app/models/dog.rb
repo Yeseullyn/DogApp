fa1efc227aee70eae5e3493e33d196635f1221b7
Yeseullyn Lee <LYSL1025@gmail.com>
2024-10-31 20:19:36 -0500

    Added one more validation for Dog model. Additionaly, modified context of about page.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,6 +4,7 @@ class Dog < ApplicationRecord
   has_many :dog_owners
   has_many :owners, through: :dog_owners
 
+  validates :name, presence: true, uniqueness: true
   validates :image_url, presence: true
   validates :breed, presence: true
 end
